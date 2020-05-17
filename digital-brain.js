4341e9483f4719f97f49614a9155feb9cfaa0533
benjamin-means <ben.w.means@gmail.com>
2020-05-18 09:25:58 +1200

    changed style to iphone calc

--- digital-brain.js
+++ digital-brain.js
@@ -1,5 +1,20 @@
-document.addEventListener("click", myFunction);
+window.addEventListener("click", calculate);
 
-function myFunction() {
-  console.log("javascript is linked up");
+var total = 0;
+var entries = [];
+
+function calculate(event) {
+  var btn = event.target.firstChild.data;
+  var output = document.getElementById("answer");
+  console.log(btn);
+  // console.log(typeof btn); they're all strings at this stage
+  entries.push(output.value);
+  console.log(entries);
+  output.value = btn;
 }
+
+// When clicking on button recognise it aa correct datatype.
+// set times and divide to do what they're suppsoed to * && /
+// push if its a number push to entries.
+// then a symbol will decide what to do with it.
+// equals will do the calc
